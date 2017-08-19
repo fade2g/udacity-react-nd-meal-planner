b94424293898edce0409559065f127600c85797b
fade2g <holger.heymanns@gmail.com>
2017-08-19 20:13:33 +0200

    feature: add middleware (logger)

--- src/index.js
+++ src/index.js
@@ -3,18 +3,25 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
-import { createStore } from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import reducer from './reducer'
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
+import { createLogger } from 'redux-logger'
+
+const logger = createLogger({});
+
+/* __REDUX_... is special for the dev tools */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(logger))
 );
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App/>
   </Provider>,
   document.getElementById('root')
 );
